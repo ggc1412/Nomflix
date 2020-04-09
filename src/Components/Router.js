36933b27dd079c896635e497373b3451eb86ddd9
ggc1412 <ggc1412@gmail.com>
2020-04-09 20:43:28 +0900

    :heavy_plus_sign:BrowserRouter, Composition, Redirct, Switch

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,22 +1,23 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import Home from "Routes/Home";
 import TV from "Routes/TV"
 import Search from "Routes/Search"
-// reacht router를 Dom으로 쓸 건데 HashRouter로 할 거고, 이건 Router랑 Route에 쓸 거야.
+// reach router를 Dom으로 쓸 건데 BrowserRouter로 할 거고, 이건 Router랑 Route에 쓸 거야.
 
 export default () =>(
     // Router라는 component 덩어리의 큰 component를 만든다.
     // Route는 Router안에서만 사용이 가능하다.
     <Router> 
         {/* 하나의 component만 리턴할 수 있기 때문에 Fragments를 사용한다. */}
-        <> 
+        <Switch> 
             {/* 어떤 url에서 어떤 Route를 render할 것인지 알려주고, 
             이 Route에는 어떤 component가 연결되어있는지 설정한다. */}
             <Route path="/" exact component={Home} />
-            <Route path="/tv" exact component={TV} />
-            <Route path="/search" exact component={Search} />
-        </>
+            <Route path="/tv" component={TV} />
+            <Route path="/search" component={Search} />
+            <Redirect from="*" to="/" />
+        </Switch>
     </Router>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "Components/App"; //.env에 설정했기에 이렇게도 접근 가능
+import App from "Components/App";
+// 절대 경로로 써줌 .env에 설정했기에 이렇게도 접근 가능
 
 ReactDOM.render(
   <React.StrictMode>
